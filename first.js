7bbacb143346d3df36345eeb104e50a6ba606667
NAJEEBKHAN123 <najeebkhanlaku.com>
2024-02-18 13:54:26 +0500

    add

--- first.js
+++ first.js
@@ -12,37 +12,13 @@
  
 let quote = ["No one can make you feel inferior without your consent.", "Not all those who wander are lost.", "Nothing is certain except for death and taxes.","That's one small step for a man, a giant leap for mankind.","The love of money is the root of all evil."];
 function quoteFunction(){
-  const randomIndex = Math.floor(math.random() * quote.length);
+  const randomIndex = Math.floor(Math.random() * quote.length);
   const randomQut = quote[randomIndex];
-  return quoteFunction;
+  console.log(randomQut)
 }
-const randomQuotes = quoteFunction();
-// quoteFunction(); 
-console.log(quoteFunction);
+quoteFunction();
 
-  // const quotes = [
-  //           "The only limit to our realization of tomorrow will be our doubts of today. - Franklin D. Roosevelt",
-  //           "The future belongs to those who believe in the beauty of their dreams. - Eleanor Roosevelt",
-  //           "Success is not final, failure is not fatal: It is the courage to continue that counts. - Winston S. Churchill",
-  //           "Do not wait to strike till the iron is hot, but make it hot by striking. - William Butler Yeats",
-  //           "The only way to do great work is to love what you do. - Steve Jobs"
-  //           // Add more quotes as needed
-  //       ];
-
-  //       // Function to generate a random quote
-  //       function generateQuote() {
-  //           // Generate a random index to select a quote from the array
-  //           const randomIndex = Math.floor(Math.random() * quotes.length);
-  //           // Retrieve the quote at the random index
-  //           const randomQuote = quotes[randomIndex];
-  //           return randomQuote;
-  //       }
-
-  //       // Display a random quote on the webpage
-  //       const quoteDisplay = document.getElementById('quoteDisplay');
-  //       // quoteDisplay.textContent = generateQuote();
-  //       console.log(generateQuote)
-
+ 
 // **************Random Number Generator: **********
 
   // function RandomNumber(){
